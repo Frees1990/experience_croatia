af74c33cb9b5da47002bc8dc22b7f1777822efe5
Fair Flower Rees <fairmejor@gmail.com>
2024-08-09 09:21:17 +0000

    Add route and function for logout button

--- app.py
+++ app.py
@@ -152,6 +152,22 @@ def profile(username):
     return render_template("profile.html", name=username)
 
 
+# LOGOUT
+@app.route("/logout")
+def logout():
+    """
+    Removes user session.
+
+    Redirects to login page
+
+    """
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
