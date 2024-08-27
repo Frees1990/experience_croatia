419c5db8b332610fb860025db22c75184369057d
Fair Flower Rees <fairmejor@gmail.com>
2024-08-27 15:54:26 +0000

    Add another code for the drop for the navigator that I miss adding

--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,54 @@ $(document).ready(function () {
     initialiseModal();
     initialiseToolTipped();
     initialiseDropdown();
+    manageAccount();
 });
+
+
+function selectForm() {
+  const elems = document.querySelectorAll('select');
+  M.FormSelect.init(elems);
+
+  // Credits to Tim Nelson for helping fix Materialize select bug
+  // Wait for the Materialize initialization to complete
+  setTimeout(() => {
+    // Select each .select-wrapper.input-field and swap the label and ul if needed
+    $('.select-wrapper.input-field').each(function () {
+      const $parentDiv = $(this);
+
+      // get the label and ul within the parent div
+      const $label = $parentDiv.children('label');
+      const $ul = $parentDiv.children('ul.select-dropdown');
+      const $caret = $parentDiv.find('svg.caret');
+      const $input = $parentDiv.find('input.select-dropdown');
+
+      // move the label before the ul if it's not already
+      if ($label.length && $ul.length && $label.next()[0] !== $ul[0]) {
+        $label.insertBefore($ul);
+      }
+
+      // Ensure the caret triggers the dropdown
+      if ($caret.length && $input.length) {
+        $caret.on('click', function () {
+          $input.trigger('click');
+        });
+      }
+
+      // Close the dropdown when an option is clicked
+      $ul.children('li').on('click', function () {
+        $input.trigger('click');
+      });
+    });
+  }, 0);
+}
+/** Initialisation of sidenav*/
+function sideNav() {
+  $('.hamburger').on("click", function () {
+    $(".mobile-menu").toggleClass("open");
+  });
+}
+
+
 /** Initialisation of Materialize dropdown elements*/
 function initialiseDropdown() {
     const elems = document.querySelectorAll('.dropdown-trigger');
@@ -15,42 +62,7 @@ function initialiseDropdown() {
         coverTrigger: false,
     });
   }
-function selectForm() {
-    const elems = document.querySelectorAll('select');
-    M.FormSelect.init(elems);
-  
-    // Credits to Tim Nelson for helping fix Materialize select bug
-    // Wait for the Materialize initialization to complete
-    setTimeout(() => {
-      // Select each .select-wrapper.input-field and swap the label and ul if needed
-      $('.select-wrapper.input-field').each(function () {
-        const $parentDiv = $(this);
-  
-        // get the label and ul within the parent div
-        const $label = $parentDiv.children('label');
-        const $ul = $parentDiv.children('ul.select-dropdown');
-        const $caret = $parentDiv.find('svg.caret');
-        const $input = $parentDiv.find('input.select-dropdown');
-  
-        // move the label before the ul if it's not already
-        if ($label.length && $ul.length && $label.next()[0] !== $ul[0]) {
-          $label.insertBefore($ul);
-        }
-  
-        // Ensure the caret triggers the dropdown
-        if ($caret.length && $input.length) {
-          $caret.on('click', function () {
-            $input.trigger('click');
-          });
-        }
-  
-        // Close the dropdown when an option is clicked
-        $ul.children('li').on('click', function () {
-          $input.trigger('click');
-        });
-      });
-    }, 0);
-  }
+
   
 /** Initialisation of Materialize datepicker elements*/
 function datepicker() {
@@ -83,6 +95,18 @@ function initialiseModal() {
 function initialiseToolTipped() {
   const elems = document.querySelectorAll('.tooltipped');
   const instances = M.Tooltip.init(elems);
+}
+
+/** Parent link(#manage-account) also changes while child links are hovered */
+function manageAccount() {
+  $(".manage-account-link").hover(
+    function () {
+      $("#manage-account").addClass("hovered");
+    },
+    function () {
+      $("#manage-account").removeClass("hovered");
+    }
+  );
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -118,4 +142,9 @@ $('#message').on('input', function () {
 
   this.style.height =
       (this.scrollHeight) + 'px';
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
 });
