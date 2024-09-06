5983d2b5eb5743a9cc8fc5e80b9fce89c5731fcb
Fair Flower Rees <fairmejor@gmail.com>
2024-09-06 11:09:18 +0000

    no changes on here, checked and tested the modal code

--- static/js/script.js
+++ static/js/script.js
@@ -148,3 +148,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems, options);
 });
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
